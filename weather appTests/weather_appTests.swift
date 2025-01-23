33b199f3a500e6b1cf4706fa6e234f0ab9c3073d
ju.se.jinglinli.ioslab <irenelijinglin@gmail.com>
2025-01-23 11:26:59 +0100

    Initial Commit

---
+++ weather appTests/weather_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  weather_appTests.swift
+//  weather appTests
+//
+//  Created by Jinglin Li on 2025-01-23.
+//
+
+import Testing
+
+struct weather_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
